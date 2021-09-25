99aa87473f1e214dd31bc4f4eef420a1dee6ff14
likui628 <likui.it@gmail.com>
2021-09-25 09:57:18 +0800

    config VTU

--- jest.config.js
+++ jest.config.js
@@ -3,8 +3,11 @@ module.exports = {
     globals: {},
     testEnvironment: 'jsdom',
     transform: {
-        "^.+\\.vue$": "vue-jest",
+        "^.+\\.vue$": "vue3-jest",
         "^.+\\js$": "babel-jest"
     },
-    moduleFileExtensions: ['vue', 'js', 'json', 'jsx', 'ts', 'tsx', 'node']
+    moduleFileExtensions: ['vue', 'js', 'json', 'jsx', 'ts', 'tsx', 'node'],
+    setupFilesAfterEnv: [
+        "<rootDir>/src/setup-test.ts"
+    ]
 }
