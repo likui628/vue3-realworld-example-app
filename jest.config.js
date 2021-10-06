6b42ea4363ad3b79a6db2ff9c389644cbd32aedb
likui628 <likui.it@gmail.com>
2021-10-06 10:13:15 +0800

    update test dependencies

--- jest.config.js
+++ jest.config.js
@@ -3,8 +3,13 @@ module.exports = {
     globals: {},
     testEnvironment: 'jsdom',
     transform: {
-        "^.+\\.vue$": "vue-jest",
-        "^.+\\js$": "babel-jest"
+      '^.+\\.vue$': 'vue3-jest',
+      '^.+\\.tsx?$': 'ts-jest',
     },
-    moduleFileExtensions: ['vue', 'js', 'json', 'jsx', 'ts', 'tsx', 'node']
-}
+    moduleFileExtensions: ['vue', 'js', 'json', 'jsx', 'ts', 'tsx', 'node'],
+    moduleNameMapper: {
+      '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
+        '<rootDir>/src/utils/fileTransformer.js',
+    },
+  }
+  
