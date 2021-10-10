d1e8249fc69c66aa8cae93415314bb89cecd38ef
likui628 <likui.it@gmail.com>
2021-10-10 09:51:35 +0800

    update PopularTags test

--- src/components/__test__/PopularTags.spec.ts
+++ src/components/__test__/PopularTags.spec.ts
@@ -2,21 +2,41 @@ import { flushPromises, mount } from '@vue/test-utils'
 import PopularTags from '../PopularTags.vue'
 import asyncComponentWrapper from '../../utils/async-component-wrapper'
 import { router } from '../../router'
+import { useTags } from '../../composable/useTags'
+import { ref } from 'vue'
 
-const mockTags = { tags: ['tag1', 'tag2', 'tag3'] }
-jest.mock('../../utils/request', () =>
-  jest.fn().mockImplementation(() => {
-    return { get: () => Promise.resolve(mockTags) }
+jest.mock('src/composable/useTags')
+const mockUseTags = useTags as jest.MockedFunction<typeof useTags>
+
+const mockValue = async (tags: Array<string>) => {
+  mockUseTags.mockReturnValueOnce({
+    tags: ref([...tags]),
+    fetchTags: jest.fn(),
   })
-)
+  await router.push('/')
+}
 
 describe('PopularTags.vue', () => {
-  it('there are three tags', async () => {
+  test('no tag there', async () => {
+    await mockValue([])
     const wrapper = mount(asyncComponentWrapper(PopularTags), {
       global: {
         plugins: [router],
       },
     })
+
+    await flushPromises()
+    expect(wrapper.html()).toContain('No tags are here... yet.')
+  })
+
+  test('there are three tags', async () => {
+    await mockValue(['foo', 'bar', 'foobar'])
+    const wrapper = mount(asyncComponentWrapper(PopularTags), {
+      global: {
+        plugins: [router],
+      },
+    })
+
     await flushPromises()
     expect(wrapper.findAll('a')).toHaveLength(3)
   })
