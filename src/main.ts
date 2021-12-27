532129a00731b2f8f1f1b7bd0606cfaf99731cea
likui628 <likui.it@gmail.com>
2021-12-27 18:03:09 +0800

    chore: switch vuex to pinia

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,13 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
-import { store, key } from './store'
 import { router } from './router'
+
+import { createPinia } from 'pinia'
 
 const app = createApp(App)
 
-app.use(store, key)
+app.use(createPinia())
 app.use(router)
 
 app.mount('#app')
---
+++ src/store/user.ts
@@ -0,0 +1,19 @@
+import { defineStore } from 'pinia'
+import { request } from '../services'
+
+export const UserStore = defineStore('user', {
+  state: () => ({
+    user: null as User | null,
+  }),
+  actions: {
+    updateUser(user: User) {
+      if (user === undefined || user === null) {
+        this.user = null
+        request.deleteAuthorizationHeader()
+      } else {
+        this.user = user
+        request.setAuthorizationHeader(user.token)
+      }
+    },
+  },
+})
