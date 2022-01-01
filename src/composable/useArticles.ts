bd2fd570ccef109597deb15e6d2528c0f1c59690
likui628 <likui.it@gmail.com>
2022-01-01 20:03:27 +0800

    feat: get articles by tag

--- src/composable/useArticles.ts
+++ src/composable/useArticles.ts
@@ -1,12 +1,9 @@
-import { ref, watch } from 'vue'
+import { ref, watch, computed } from 'vue'
 import { useRoute } from 'vue-router'
-import { getArticles } from '../services/article/getArticles'
+import { getArticles, getArticlesByTag } from '../services/article/getArticles'
 
 export function useArticles() {
-  const page = ref(1)
-  async function changePage(value: number) {
-    page.value = value
-  }
+  const route = useRoute()
 
   const articles = ref<Article[]>([])
   const articlesCount = ref(0)
@@ -14,7 +11,13 @@ export function useArticles() {
   async function fetchArticles(): Promise<void> {
     let responsePromise: null | Promise<ArticlesResponse> = null
 
-    responsePromise = getArticles(page.value)
+    if (route.name === 'tag' && tag.value !== undefined) {
+      responsePromise = getArticlesByTag(tag.value, page.value)
+    }
+
+    if (route.name === 'global-feed') {
+      responsePromise = getArticles(page.value)
+    }
 
     if (responsePromise !== null) {
       const response = await responsePromise
@@ -22,18 +25,16 @@ export function useArticles() {
       articlesCount.value = response.articlesCount
     }
   }
-  const route = useRoute()
-  const tag = ref('')
-  watch(
-    () => route.params.tag,
-    (tagParam) => {
-      if (tagParam !== tag.value) {
-        tag.value = typeof tagParam === 'string' ? tagParam : ''
-      }
-    },
-    { immediate: true }
-  )
 
+  const tag = computed(() =>
+    typeof route.params.tag === 'string' ? route.params.tag : ''
+  )
+  watch(tag, fetchArticles)
+
+  const page = ref(1)
+  async function changePage(value: number) {
+    page.value = value
+  }
   watch(page, fetchArticles)
 
   return {
--- src/services/article/getArticles.ts
+++ src/services/article/getArticles.ts
@@ -2,6 +2,13 @@ import { limit, request } from '../index'
 
 export function getArticles(page = 1): Promise<ArticlesResponse> {
   const params = { limit, offset: (page - 1) * limit }
-  return request
-    .get<ArticlesResponse>('/articles', { params })
+  return request.get<ArticlesResponse>('/articles', { params })
+}
+
+export function getArticlesByTag(
+  tagName: string,
+  page = 1
+): Promise<ArticlesResponse> {
+  const params = { tag: tagName, limit, offset: (page - 1) * limit }
+  return request.get<ArticlesResponse>('/articles', { params })
 }
