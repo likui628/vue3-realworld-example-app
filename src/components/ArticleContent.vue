210acc1dad26e3f5d00fa093eb99e82abe21ed3d
likui628 <likui.it@gmail.com>
2022-01-12 16:51:00 +0800

    chore: article components structure

---
+++ src/components/ArticleContent.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="col-md-12">
+    <p>
+      Web development technologies have evolved at an incredible clip over the
+      past few years.
+    </p>
+    <h2 id="introducing-ionic">Introducing RealWorld.</h2>
+    <p>It's a great solution for learning how other frameworks work.</p>
+  </div>
+</template>
+<script lang="ts" setup></script>
---
+++ src/components/ArticleDetail.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="banner">
+    <div class="container">
+      <h1>How to build webapps that scale</h1>
+      <div class="article-meta">
+        <ArticleMeta />
+      </div>
+    </div>
+  </div>
+
+  <div class="container page">
+    <div class="row article-content">
+      <ArticleContent />
+    </div>
+
+    <hr />
+
+    <div class="article-actions">
+      <div class="article-meta">
+        <ArticleMeta />
+      </div>
+    </div>
+
+    <div class="row">
+      <div class="col-xs-12 col-md-8 offset-md-2">
+        <suspense>
+          <template #default>
+            <ArticleComments :slug="slug" />
+          </template>
+          <template #fallback>
+            <div class="card">
+              <div class="card-block">Loading comments...</div>
+            </div>
+          </template>
+        </suspense>
+      </div>
+    </div>
+  </div>
+</template>
+<script lang="ts" setup>
+import ArticleMeta from '../components/ArticleMeta.vue'
+import ArticleContent from '../components/ArticleContent.vue'
+import ArticleComments from '../components/ArticleComments.vue'
+import { useRoute } from 'vue-router'
+
+import { getArticleBySlug } from '../services/article/getArticle'
+const route = useRoute()
+const slug = route.params.slug as string
+</script>
