2d5b13e50db1655464e9c1df7a0a6f26ba22922c
likui628 <likui.it@gmail.com>
2022-01-19 15:47:20 +0800

    chore: initializing tags with []

--- src/composable/useTags.ts
+++ src/composable/useTags.ts
@@ -2,7 +2,7 @@ import { ref } from 'vue'
 import { getAllTags } from '../services/tag/getTags'
 
 export function useTags() {
-  const tags = ref<string[]>()
+  const tags = ref<string[]>([])
 
   async function fetchTags(): Promise<void> {
     tags.value = await getAllTags()
