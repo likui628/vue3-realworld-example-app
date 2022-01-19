a499590786dadc929c13270797b0f43717079703
likui628 <likui.it@gmail.com>
2022-01-19 14:49:52 +0800

    chore: String => string

--- src/composable/useTags.ts
+++ src/composable/useTags.ts
@@ -2,9 +2,9 @@ import { ref } from 'vue'
 import { getAllTags } from '../services/tag/getTags'
 
 export function useTags() {
-  const tags = ref<String[]>([])
+  const tags = ref<string[]>()
+
   async function fetchTags(): Promise<void> {
-    tags.value = []
     tags.value = await getAllTags()
   }
 
