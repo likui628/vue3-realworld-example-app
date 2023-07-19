3fd61a79b846e50a10787d53c4e6658c14c383d8
Li Kui <90845831+likui628@users.noreply.github.com>
2023-07-19 17:57:42 +0800

    fix: deploy failed

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,4 @@
 export const CONFIG = {
-  API_HOST: import.meta.env.VITE_API_HOST || '',
+  API_HOST: (import.meta.env.VITE_API_HOST || '') as string,
   DEFAULT_AVATAR: 'https://api.realworld.io/images/smiley-cyrus.jpeg',
 }
